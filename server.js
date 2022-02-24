01bfd90753a224dda40b6e4c6e3e3438de79f5df
danieldamian09 <humbertorivero9@gmail.com>
2022-02-24 16:55:44 -0300

    add cors

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const multer = require("multer")
+const cors = require("cors")
 // const mimeTypes = require("mime-types")
 const app = express();
+
+app.use(cors());
 
 // configuracion para l extension de la imagen
 const storage = multer.diskStorage({
