5a05f534934f78cd3e61b61d2d326c2bdd030ab1
danieldamian09 <humbertorivero9@gmail.com>
2022-02-24 13:06:47 -0300

    setting multer

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require("express");
+const res = require("express/lib/response");
+const multer = require("multer")
 const app = express();
+
+// configuracion de multer
+const upload = multer({
+  dest:"uploads/"
+})
 
 app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/views/index.html")
 });
 
 // Permitir que el usuario nos envie archivos
-app.post("/file", (req, resp) => {
-  
+app.post("/file",upload.single("avatar"), (req, resp) => {
+  res.send("Archivo subido con exito")
 })
 
 app.listen(8000, () => console.log("Server started"));
