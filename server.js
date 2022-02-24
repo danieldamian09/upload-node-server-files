dc6acfb2eb20a581d2f94b5e9556628a64b7b9d3
danieldamian09 <humbertorivero9@gmail.com>
2022-02-24 12:59:23 -0300

    create form

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send("Hola Mundo");
+	res.sendFile(__dirname + "/views/index.html")
 });
+
+// Permitir que el usuario nos envie archivos
+app.post("/file", (req, resp) => {
+  
+})
 
 app.listen(8000, () => console.log("Server started"));
